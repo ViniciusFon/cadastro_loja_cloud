e4c31ee317934040a1d861865c88acfbca114540
viniciusFon <vinicius.fontana@gmail.com>
2024-02-28 19:24:37 -0300

    Alterado docker compose retirando o nginx

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,58 +1,60 @@
-resource "aws_vpc" "loja_vpc" {
+resource "aws_vpc" "loja_vpc_1" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name = "loja_vpc"
+    "Name" = "loja_vpc_1"
   }
 }
 
-resource "aws_subnet" "loja_subnet" {
-  vpc_id                  = aws_vpc.loja_vpc.id
+
+resource "aws_subnet" "loja_subnet_pub_1a" {
+  vpc_id                  = aws_vpc.loja_vpc_1.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "loja_subnet_public"
+    "Name" = "loja_subnet_pub_1a"
   }
+
 }
 
-resource "aws_internet_gateway" "loja_igw" {
-  vpc_id = aws_vpc.loja_vpc.id
+resource "aws_internet_gateway" "loja_igw_1a" {
+  vpc_id = aws_vpc.loja_vpc_1.id
 
   tags = {
-    Name = "loja_igw"
+    "Name" = "loja_igw_1a"
   }
 }
 
-resource "aws_route_table" "loja_rt" {
-  vpc_id = aws_vpc.loja_vpc.id
+resource "aws_route_table" "loja_rtb_pub" {
+  vpc_id = aws_vpc.loja_vpc_1.id
 
   tags = {
-    Name = "loja_rt"
+    "Name" = "loja_rtb_pub"
   }
 }
 
-resource "aws_route" "loja_route" {
-  route_table_id         = aws_route_table.loja_rt.id
+resource "aws_route" "loja_default_rtb" {
+  route_table_id         = aws_route_table.loja_rtb_pub.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.loja_igw.id
+  gateway_id             = aws_internet_gateway.loja_igw_1a.id
 }
 
-resource "aws_route_table_association" "loja_rtb_association" {
-  route_table_id = aws_route_table.loja_rt.id
-  subnet_id      = aws_subnet.loja_subnet.id
+resource "aws_route_table_association" "loja_rtba_pub_1a" {
+  route_table_id = aws_route_table.loja_rtb_pub.id
+  subnet_id      = aws_subnet.loja_subnet_pub_1a.id
 }
 
-resource "aws_instance" "loja_ec2_instance" {
-  instance_type          = "t2.micro"
-  key_name               = "loja_key"
+
+resource "aws_instance" "loja_ec2_inst" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.loja_ami.id
+  key_name               = aws_key_pair.loja_key.id
   vpc_security_group_ids = [aws_security_group.loja_sg.id]
-  subnet_id              = aws_subnet.loja_subnet.id
-  ami                    = data.aws_ami.loja_ami.id
-
+  subnet_id              = aws_subnet.loja_subnet_pub_1a.id
   user_data = file("userdata.tpl")
 
   root_block_device {
@@ -60,7 +62,6 @@ resource "aws_instance" "loja_ec2_instance" {
   }
 
   tags = {
-    Name = "loja_ec2_instance"
+    "Name" = "loja_ec2_inst"
   }
-
 }
--- deploy/terraform/security.tf
+++ deploy/terraform/security.tf
@@ -1,34 +1,31 @@
+
 resource "aws_security_group" "loja_sg" {
   name        = "loja_sg"
-  description = "Loja Security Group"
-  vpc_id      = aws_vpc.loja_vpc.id
+  description = "loja security group"
+  vpc_id      = aws_vpc.loja_vpc_1.id
+
+  tags = {
+    "Name" = "loja_sg"
+  }
 }
 
-resource "aws_security_group_rule" "loja_sg_public_out" {
-  from_port         = 0
-  protocol          = "-1"
+resource "aws_security_group_rule" "public_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
   security_group_id = aws_security_group.loja_sg.id
-  to_port           = 0
-  type              = "egress"
-  cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "loja_sg_https_in" {
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.loja_sg.id
-  to_port           = 443
+resource "aws_security_group_rule" "public_in_ssh" {
   type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "loja_sg_ssh_in" {
   from_port         = 22
-  protocol          = "tcp"
-  security_group_id = aws_security_group.loja_sg.id
   to_port           = 22
-  type              = "ingress"
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.loja_sg.id
 }
 
 resource "aws_key_pair" "loja_key" {
@@ -36,3 +33,11 @@ resource "aws_key_pair" "loja_key" {
   public_key = file("~/.ssh/loja_key.pub")
 }
 
+resource "aws_security_group_rule" "public_in_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.loja_sg.id
+}
